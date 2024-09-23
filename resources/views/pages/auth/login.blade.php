bd16d4205c2e80b6bf459ad7817ed58b73a19e3c
tyoo234 <prasetyoo234@gmail.com>
2024-09-23 07:52:19 +0700

    master

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -16,43 +16,48 @@
 
         <div class="card-body">
             <form method="POST"
-                action="#"
+                action="{{route('login')}}"
                 class="needs-validation"
                 novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email') is-invalid
+
+                        @enderror"
                         name="email"
                         tabindex="1"
-                        required
+
                         autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
-                    </div>
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{$message}}
+                        </div>
+                        @enderror
+
                 </div>
 
                 <div class="form-group">
                     <div class="d-block">
                         <label for="password"
                             class="control-label">Password</label>
-                        <div class="float-right">
-                            <a href="auth-forgot-password.html"
-                                class="text-small">
-                                Forgot Password?
-                            </a>
-                        </div>
+
                     </div>
                     <input id="password"
                         type="password"
-                        class="form-control"
+                        class="form-control @error('password')
+                        is-invalid
+
+                        @enderror"
                         name="password"
-                        tabindex="2"
-                        required>
-                    <div class="invalid-feedback">
-                        please fill in your password
-                    </div>
+                        tabindex="2">
+
+                        @error('password')
+                        <div class="invalid-feedback">
+                            {{$message}}
+                        @enderror
                 </div>
 
                 <div class="form-group">
